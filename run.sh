8217dde59bc7c3862d55738d3071e9ce36633983
hkdkl <72981446+hkdkl@users.noreply.github.com>
2021-07-13 23:05:03 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ set -ex
 #[exec] (chromium) {'/usr/bin/chromium --no-sandbox --disable-dev-shm-usage'}
 #EOF
 
-cat >> chr << EOF
-/usr/bin/chromium --no-sandbox --disable-dev-shm-usage --disable-setuid-sandbox --no-zygote --single-process --no-pings --no-first-run --disable-accelerated-2d-canvas --disable-gpu
-EOF
+#cat >> chr << EOF
+#/usr/bin/chromium --no-sandbox --disable-dev-shm-usage --disable-setuid-sandbox --no-zygote --single-process --no-pings --no-first-run --disable-accelerated-2d-canvas --disable-gpu
+#EOF
 
 exec supervisord -c /app/supervisord.conf
